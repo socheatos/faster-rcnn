4f56fcb3640b1efe1b49977ffe2fe1715f2be748
socheatos <chansocheatasea@gmail.com>
2022-06-15 13:06:27 -0400

    ensure all variables are on the same device

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from data.Dataset import ImageDataset
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import torch
-from data.utils import collate_fn, collate_fn_2
 from config import Config
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -15,7 +14,7 @@ transform = transforms.Compose([transforms.ToPILImage(),
                                 transforms.Resize((CONFIG.input_h,CONFIG.input_w)),
                                 transforms.ToTensor()])
 dataset = ImageDataset('data/images/','data/labels.csv',transform=transform)
-train_loader = DataLoader(dataset,batch_size=2,shuffle=True)
+train_loader = DataLoader(dataset,batch_size=CONFIG.batch_size,shuffle=True)
 
 print(f'Batches in test_set {len(train_loader)}')
 print(f'Examples in test_set {len(train_loader.sampler)}')
@@ -26,12 +25,14 @@ print(f'Examples in test_set {len(train_loader.sampler)}')
 vgg = FeatureExtractor.VGG16()
 rpn = RegionProposalNetwork.RegionProposalNetwork(config=CONFIG)
 frcnn = FasterRCNN.FasterRCNN(vgg, rpn)
+frcnn.cuda(device)
 print('done with f-rcnn')
 
 with torch.no_grad():
     for images, labels, bboxs in train_loader:
         images, labels,bboxs = images.to(device), labels.to(device), bboxs.to(device)
-        bbox_output, score_output = frcnn(images,bboxs)
+        # bbox_output, score_output = frcnn(images,bboxs)
+        outputs = frcnn(images,bboxs)
         # print(f'faster rcnn output: {bbox_output.shape, score_output.shape}')
 
         
