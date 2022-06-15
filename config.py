c7e725a210522b691791a32502e385ee106b2d64
socheatos <chansocheatasea@gmail.com>
2022-06-15 13:02:47 -0400

    added config params for proposal and anchor generation

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ class Config():
     def __init__(self,TRAIN=False):
         self.input_h = 600
         self.input_w = 600
+        self.batch_size = 2
 
         self.feat_stride = 16 # (original img dimensions)/(feature map dimension) for VGG it's 16
         self.ratios = [0.5, 1, 2]
+        # self.anchor_scales = [8, 16, 32]
         self.anchor_scales = [4, 8, 16]
-        self.anchor_base = 8 # 16
+        self.anchor_base = 16 # 16
 
         self.nms_threshold = 0.7
         self.n_train_pre_nms = 12000
@@ -20,3 +22,11 @@ class Config():
         else:
             self.pre_nms = self.n_test_pre_nms
             self.pos_nms = self.n_test_pos_nms
+
+        self.pos_ratio = 0.5
+
+        self.anchor_pos_iou_thres = 0.5
+        self.anchor_n_sample = 256
+        
+        self.proposal_pos_iou_thres = 0.7
+        self.proposal_n_sample = 128
